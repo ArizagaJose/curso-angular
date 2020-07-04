25325bf6a11d132b1255057f12b0ec2fc3764004
José Arizaga <jope.arizag@gmail.com>
2020-07-04 00:48:34 -0300

    Aplicacion reactiva

--- angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
+import { DestinosApiClient } from '../models/destinos-api-client.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -7,18 +8,31 @@ import { DestinoViaje } from '../models/destino-viaje.model';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-  destinos: DestinoViaje[];
-  constructor() {
-    this.destinos = [];
+  @Output() onItemAdded: EventEmitter<DestinoViaje>
+  updates: string[]
+
+
+  constructor(public destinosApiClient: DestinosApiClient) {
+    this.onItemAdded = new EventEmitter()
+    this.updates = []
+    this.destinosApiClient.subscribedOnChange((d: DestinoViaje) => {
+      if (d != null) {
+          this.updates.push('Se ha elegido a ' + d.n)
+      }
+    })
   }
 
   ngOnInit(): void {
   }
 
-  guardar(nombre: string, url: string): boolean {
-    this.destinos.push(new DestinoViaje(nombre,url))
-    console.log(this.destinos)
-    return false
+  agregado(d: DestinoViaje) {
+    this.destinosApiClient.add(d)
+    this.onItemAdded.emit(d)
+  }
+
+  elegido(e: DestinoViaje) {
+    this.destinosApiClient.elegir(e)
+    e.setSelected(true)
   }
 
 
--- angular-wishlist/src/app/models/destino-viaje.model.ts
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -1,9 +1,18 @@
-export class DestinoViaje{
-    nombre:string
-    imagenUrl:string
-    
-    constructor(n:string, u:string){
-        this.nombre = n
-        this.imagenUrl = u
+import {v4 as uuid} from 'uuid';
+
+export class DestinoViaje {
+    private selected: boolean
+    public servicios: string[]
+    id = uuid()
+    constructor(public n: string, public u: string) {
+        this.servicios = ["Desayuno","Almuerzo","Merienda","Cena"]
+     }
+
+    isSelected(): boolean {
+        return this.selected
+    }
+
+    setSelected(s: boolean) {
+        this.selected = s
     }
 }
